9bb912a484eda2b0108f88e968ddeda29ca1126b
David J. Harris <davharris@users.noreply.github.com>
2016-02-11 15:46:25 -0500

    mention DIC changing too

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,3 @@
 .onLoad <- function(...) {
-  packageStartupMessage("Note that predict.bayescomm has changed from version 0.1-x. See ?predict.bayescomm for details")
+  packageStartupMessage("Note that predict.bayescomm() and DIC() have changed from version 0.1-x. See ?predict.bayescomm for details")
 }
